3111793d714b9ad43e6cbf19edc57ce7684601e5
Harry Maynard <harry.maynard@maximusbc.ca>
2021-02-02 14:26:31 -0500

    Revert oop to a default vue project. Also created backup of old project.

--- oop/src/main.js
+++ oop/src/main.js
@@ -1,18 +1,12 @@
-import App from './App.vue';
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Vuelidate from 'vuelidate';
-import router from './router';
-import store from './store/store';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
-
-Vue.use(VueRouter);
-Vue.use(Vuelidate);
+Vue.config.productionTip = false
 
 new Vue({
-  el: '#app',
   router,
   store,
   render: h => h(App)
-});
+}).$mount('#app')
