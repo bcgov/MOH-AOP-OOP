eaa7ded59acd646789552f41f8cf254bd0658479
Cyril Bravo <cyril.bravo@maximusbc.ca>
2021-03-01 14:53:40 -0800

    Added move info attributes of form.js

--- oop/src/store/modules/form.js
+++ oop/src/store/modules/form.js
@@ -11,6 +11,15 @@ export const SET_PHN = 'setPhn';
 export const SET_EMAIL = 'setEmail';
 export const SET_PHONE = 'setPhone';
 
+// Move Info page
+export const SET_MOVE_FROM_BC_DATE = 'setMoveFromBCDate';
+export const SET_ARRIVE_DESTINATION_DATE = 'setArriveDestinationDate';
+export const SET_COUNTRY = 'setCountry';
+export const SET_ADDRESS_LINE = 'setAddressLine';
+export const SET_PROVINCE = 'setProvince';
+export const SET_CITY = 'setCity';
+export const SET_POSTAL_CODE = 'setPostalCode';
+
 // Sending page:
 export const SET_SUBMISSION_RESPONSE = 'setSubmissionResponse';
 export const SET_SUBMISSION_ERROR = 'setSubmissionError';
@@ -23,6 +32,13 @@ export default {
       phn: null,
       email: null,
       phone: null,
+      moveFromBCDate: null,
+      arriveDestinationDate: null,
+      country: null,
+      addressLine: null,
+      province: null,
+      city: null,
+      postalCode: null,
       submissionResponse: null,
       submissionError: null,
     };
@@ -44,6 +60,27 @@ export default {
     setPhone(state, payload) {
       state.phone = payload;
     },
+    setMoveFromBCDate(state, payload) {
+      state.moveFromBCDate = payload;
+    },
+    setArriveDestinationDate(state, payload) {
+      state.arriveDestinationDate = payload;
+    },
+    setCountry(state, payload) {
+      state.country = payload;
+    },
+    setAddressLine(state, payload) {
+      state.addressLine = payload;
+    },
+    setProvince(state, payload) {
+      state.province = payload;
+    },
+    setCity(state, payload) {
+      state.city = payload;
+    },
+    setPostalCode(state, payload) {
+      state.postalCode = payload;
+    },
     setSubmissionResponse(state, payload) {
       state.submissionResponse = payload;
     },
@@ -57,6 +94,13 @@ export default {
       commit(SET_PHN, null);
       commit(SET_EMAIL, null);
       commit(SET_PHONE, null);
+      commit(SET_MOVE_FROM_BC_DATE, null);
+      commit(SET_ARRIVE_DESTINATION_DATE, null);
+      commit(SET_COUNTRY, null);
+      commit(SET_ADDRESS_LINE, null);
+      commit(SET_PROVINCE, null);
+      commit(SET_CITY, null);
+      commit(SET_POSTAL_CODE, null);
       commit(SET_SUBMISSION_RESPONSE, null);
       commit(SET_SUBMISSION_ERROR, null);
     },
@@ -71,6 +115,27 @@ export default {
     },
     setPhone({ commit }, phone) {
       commit(SET_PHONE, phone);
+    },
+    setMoveFromBCDate({ commit }, moveFromBCDate) {
+      commit(SET_MOVE_FROM_BC_DATE, moveFromBCDate);
+    },
+    setArriveDestinationDate({ commit }, arriveDestinationDate) {
+      commit(SET_ARRIVE_DESTINATION_DATE, arriveDestinationDate);
+    },
+    setCountry({ commit }, country) {
+      commit(SET_COUNTRY, country);
+    },
+    setAddressLine({ commit }, addressLine) {
+      commit(SET_ADDRESS_LINE, addressLine);
+    },
+    setProvince({ commit }, province) {
+      commit(SET_PROVINCE, province);
+    },
+    setCity({ commit }, city) {
+      commit(SET_CITY, city);
+    },
+    setPostalCode({ commit }, postalCode) {
+      commit(SET_POSTAL_CODE, postalCode);
     },
     setApiResponse({ commit }, response) {
       commit(SET_SUBMISSION_RESPONSE, response);
