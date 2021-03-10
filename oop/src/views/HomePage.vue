ce962e416cad30eb972bd2155e023981cee996e8
Harry Maynard <harry.maynard@maximusbc.ca>
2021-03-10 13:56:59 -0500

    OOPDEF-113: Updated home page text.

--- oop/src/views/HomePage.vue
+++ oop/src/views/HomePage.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="home">
     <PageContent>
-      <div class="container pt-3 pt-sm-5 mb-3">
+      <div class="container pt-3 pt-sm-5 mb-5">
         <h1>MSP permanent move outside of British Columbia</h1>
         <hr/>
         <h2>Who can complete this form</h2>
         <p>If you are permanently moving out of British Columbia (B.C.), you must notify Health Insurance BC (HIBC) of your departure date and your new address.</p>
         <ul>
-          <li><b>If you are moving within Canada:</b> coverage is provided for the balance of the month in which you leave the province plus two consecutive months. If required, coverage may be extended for up to three extra months to cover you while in transit. Upon arrival, you should immediately apply to the health plan of the new province or territory.</li> <br>
-          <li><b>If you are moving outside Canada:</b> coverage is provided for the balance of the month in which you leave the province.</li>
+          <li><b>If you are moving within Canada:</b> coverage is provided for the remainder of the month in which you leave the province plus the next two months. If required, your coverage may be extended for up to three extra months to cover you while in transit. Upon arrival, you should immediately apply to the health plan of the new province or territory.</li>
+          <li><b>If you are moving outside Canada:</b> coverage is provided for the remainder of the month in which you leave the province.</li>
         </ul>
         <br/>
         <div class="container">
           <div class="row">
             <div class="col-md-6">
               <h2>Requirements</h2>
-              <p>Personal Health Number (PHN), which is the 10 digit number that can be found on the back of the BC Services Card (or front of the CareCard if you still have one).</p>
+              <p>Your Personal Health Number (PHN) is the 10 digit number that can be found on the back of your BC Services Card (or front of your CareCard if you still have one).</p>
             </div>
             <div class="col-md-6">
               <img
@@ -29,7 +29,7 @@
         <br/>
         <div class="container">
           <h2>What happens after completing this form</h2>
-          <p>After completing the form, Health Insurance BC will send a confirmation letter to your address on file. It is important to provide a complete and accurate new address in this form. After you submit the form, HIBC will send a confirmation letter to your new address. HIBC will use the new address to contact you with any further correspondence.</p>
+          <p>HIBC will process your request. We will contact you by email (if provided) or mail if there are any issues cancelling your MSP coverage. A confirmation letter will be sent to your new address following cancellation of  coverage.</p>
           <br>
           <h2>Time required to complete this form</h2>
           <p>10-15 minutes</p>
