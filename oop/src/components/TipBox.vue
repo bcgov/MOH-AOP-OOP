0eb204a5a16780d4e0047145adb04e7842687538
Harry Maynard <harry.maynard@maximusbc.ca>
2021-03-14 18:46:37 -0400

    OOPDEF-51: Added "Tip" box to "Your Info" page.

---
+++ oop/src/components/TipBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :class="'tip-container rounded p-3' + className">
+    <div>
+      <h4>{{ title }}</h4>
+    </div>
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TipBox',
+  props: {
+    title: {
+      type: String,
+      default: 'Tip',
+    },
+    className: {
+      type: String,
+      default: '',
+    }
+  }
+}
+</script>
+
+<style scoped>
+.tip-container {
+  background: #f2f2f2;
+}
+</style>
