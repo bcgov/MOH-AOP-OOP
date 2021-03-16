6ddddf10fa99f5e6cb723b64348116f383c9ecd5
Harry Maynard <harry.maynard@maximusbc.ca>
2021-03-16 15:01:12 -0400

    OOP: Added submission error page.

--- oop/src/views/SubmissionErrorPage.vue
+++ oop/src/views/SubmissionErrorPage.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="submission-error">
-    <h1>Page content here.</h1>
+  <div>
+    <PageContent>
+      <div class="container pt-3 pt-sm-5 mb-3">
+
+        <div class="box-border border border-danger rounded">
+          <div class="row align-items-center">
+            <div class="col-2 pr-0 text-center">
+              <div class="status-icon text-danger">
+                <font-awesome-icon icon="times-circle" />
+              </div>
+            </div>
+            <div class="col-10 pl-0 py-3">
+              <h1>There was a technical issue with your submission</h1>
+              <p>Your application was not submitted.</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </PageContent>
   </div>
 </template>
+
+<script>
+import PageContent from '../components/PageContent.vue';
+
+export default {
+  name: 'SubmissionErrorPage',
+  components: {
+    PageContent,
+  }
+}
+</script>
+
+<style scoped>
+.box-border {
+  border-width: 4px !important;
+}
+.status-icon {
+  font-size: 32px;
+}
+</style>
