675560264640758041487aad943d0b382149a1e0
Cyril Bravo <cyril.bravo@maximusbc.ca>
2021-03-17 14:40:58 -0700

    Fixed bug on missing address line

--- oop/src/views/MoveInfoPage.vue
+++ oop/src/views/MoveInfoPage.vue
@@ -189,8 +189,9 @@ export default {
       }
     }
 
+    console.log(currNumOfAddressLines);
     for (let i=currNumOfAddressLines-1; i>=0; i--){
-      if (this.addressLines[i].value == null || this.addressLines[i].value == ''){
+      if ((this.addressLines[i].value == null || this.addressLines[i].value == '') && currNumOfAddressLines > 1){
         this.addressLines.splice(i,1);
       }
     }
