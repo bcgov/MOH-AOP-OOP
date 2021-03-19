601f0114ac6e731bf575532fdcf330a4896dbbc7
Harry Maynard <harry.maynard@maximusbc.ca>
2021-03-19 16:33:51 -0400

    OOP: Home page alignment.

--- oop/src/views/HomePage.vue
+++ oop/src/views/HomePage.vue
@@ -13,29 +13,25 @@
           <li><b>If you are moving outside Canada:</b> coverage is provided for the remainder of the month in which you leave the province.</li>
         </ul>
         <br/>
-        <div class="container">
-          <div class="row">
-            <div class="col-md-6">
-              <h2>Requirements</h2>
-              <p>Your Personal Health Number (PHN) is the 10 digit number that can be found on the back of your <a href="https://www2.gov.bc.ca/gov/content/health/health-drug-coverage/msp/bc-residents/personal-health-identification/your-bc-services-card" target="_blank">BC Services Card</a> (or front of your CareCard if you still have one).</p>
-            </div>
-            <div class="col-md-6">
-              <img
-              alt="BC Services Card Sample"
-              src="/images/bcid-sample.jpg"
-              class="bcid"
-              />
-            </div>
+        <div class="row">
+          <div class="col-md-6">
+            <h2>Requirements</h2>
+            <p>Your Personal Health Number (PHN) is the 10 digit number that can be found on the back of your <a href="https://www2.gov.bc.ca/gov/content/health/health-drug-coverage/msp/bc-residents/personal-health-identification/your-bc-services-card" target="_blank">BC Services Card</a> (or front of your CareCard if you still have one).</p>
+          </div>
+          <div class="col-md-6">
+            <img
+            alt="BC Services Card Sample"
+            src="/images/bcid-sample.jpg"
+            class="bcid"
+            />
           </div>
         </div>
         <br/>
-        <div class="container">
-          <h2>What happens after completing this form</h2>
-          <p>HIBC will process your request. We will contact you by email (if provided) or mail if there are any issues cancelling your MSP coverage. A confirmation letter will be sent to your new address following cancellation of  coverage.</p>
-          <br>
-          <h2>Time required to complete this form</h2>
-          <p>10-15 minutes</p>
-        </div>
+        <h2>What happens after completing this form</h2>
+        <p>HIBC will process your request. We will contact you by email (if provided) or mail if there are any issues cancelling your MSP coverage. A confirmation letter will be sent to your new address following cancellation of  coverage.</p>
+        <br>
+        <h2>Time required to complete this form</h2>
+        <p>10-15 minutes</p>
       </div>
     </PageContent>
     <ContinueBar @continue='nextPage()'/>
