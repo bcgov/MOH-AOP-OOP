1d0626e7aeaf21bcefb91cdc5dbae331cf567e9a
Harry Maynard <harry.maynard@maximusbc.ca>
2021-04-01 12:15:43 -0400

    OOPDEF-104: Mobile stepper.

--- oop/src/components/ProgressBar.vue
+++ oop/src/components/ProgressBar.vue
@@ -1,62 +1,113 @@
 <template>
-  <div v-if='isCurrentPathInSteps'
-       class='progress-bar-component'>
-    <div class="scrollable-content">
-      <div class='progress-bar-container'>
-        <div class='progress-bar' :style='progressBarStyles'></div>
-      </div>
-      <div class='step-container'>
-        <a href="javascript:void(0);"
-            v-for='route in routes'
-            :key='route.path'
-            :style='getLinkStyles(route.path)'
-            @click="onClickLink(route.path)">
-          <div class="step">
-            <div class='step-text'>{{route.title}}</div>
-          </div>
-        </a>
+  <nav class="container component-container">
+    <div class="progress-bar-container">
+      <div class="progress-bar" :style="progressBarStyles"></div>
+    </div>
+    <div class="step-container">
+      <a
+        href="javascript:void(0);"
+        v-for="(route, index) in routes"
+        :key="route.path"
+        :style='getLinkStyles(route.path)'
+        @click="onClickLink(route.path)"
+      >
+        <div class="step" v-bind:class="{'step-selected': index + 1 === currentStepNumber, 'step-passed': index + 1 < currentStepNumber}">
+          <div class="step-text" v-bind:class="{'v-step-text-selected': index + 1 === currentStepNumber}">{{ route.title }}</div>
+        </div>
+      </a>
+    </div>
+    <div v-bind:class="{ hide: hideMobileStep }" class="mobile-step-container p-3 border-bottom">
+      <div class="pb-3">Step {{ currentStepNumber }}/{{ routes.length }} - {{ currentStepTitle }}</div>
+      <div class="chevron-container" @click="openDropdown">
+        <font-awesome-icon icon="chevron-down" />
       </div>
     </div>
-  </div>
+    <div v-bind:class="{ hide: hideMobileProgress }" class="mobile-progress-bar-container p-3 pt-5 border-bottom">
+      <div class="v-progress-bar-container">
+        <div class="v-progress-bar" :style="verticalProgressBarStyles"></div>
+      </div>
+      <div class="v-step-container">
+        <a
+        href="javascript:void(0);"
+        v-for="(route, index) in routes"
+        :key="route.path"
+        @click="onClickLink(route.path)"
+      >
+        <div class="v-step" v-bind:class="{'v-step-selected': index + 1 === currentStepNumber, 'v-step-passed': index + 1 < currentStepNumber}">
+          <div class="v-step-text" v-bind:class="{'v-step-text-selected': index + 1 === currentStepNumber}" >{{ route.title }}</div>
+        </div>
+      </a>
+      </div>
+      <div class="chevron-container" @click="closeDropdown">
+        <font-awesome-icon icon="chevron-up" />
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
+import { scrollTo } from '../helpers/scroll';
 import environment from '../settings';
+import {
+  MODULE_NAME,
+  SET_SHOW_MOBILE_STEPPER_DETAILS,
+} from '../store/modules/app'
 
 export default {
-  name: 'ProgressBar',
+  name: "ProgressBar",
   components: {},
   props: {
     currentPath: String,
-    routes: Array,
+    routes: Array
   },
   computed: {
+    hideMobileStep() {
+      return this.$store.state.app.showMobileStepperDetails;
+    },
+    hideMobileProgress() {
+      return !this.$store.state.app.showMobileStepperDetails;
+    },
     progressBarStyles() {
-      const index = this.routes.findIndex((element) => {
+      const index = this.routes.findIndex(element => {
         return element.path === this.currentPath;
       });
-      if (!this.routes || this.routes.length === 0) {
-        return {};
-      }
       return {
-        width: (100 / this.routes.length * index) + (100 / this.routes.length / 2) + '%'
+        width:
+          (100 / this.routes.length) * index +
+          100 / this.routes.length / 2 +
+          "%"
       };
     },
-    isCurrentPathInSteps() {
-      const index = this.routes.findIndex((element) => {
+    verticalProgressBarStyles() {
+      const index = this.routes.findIndex(element => {
         return element.path === this.currentPath;
       });
-      return index > -1;
+      return {     
+        height: index / (this.routes.length - 1) * 100 + "%"
+      };
+    },
+    currentStepNumber() {
+      const index = this.routes.findIndex(element => {
+        return element.path.includes(this.currentPath);
+      });
+      return index + 1;
+    },
+    currentStepTitle() {
+      const index = this.routes.findIndex(element => {
+        return element.path.includes(this.currentPath);
+      });
+      return this.routes[index].title;
     }
   },
   methods: {
-    onClickLink: function(path) {
+    onClickLink(path) {
       if (this.currentPath !== path && 
         (
           environment.bypassRouteGuards ||
           this.isPastPath(path)
         )) {
         this.$router.push(path);
+        scrollTo(0);
       }
     },
     isPastPath(path) {
@@ -74,38 +125,47 @@ export default {
         cursor: this.isPastPath(path) ? 'pointer' : 'default'
       }
     },
+    openDropdown() {
+      this.$store.dispatch(MODULE_NAME + '/' + SET_SHOW_MOBILE_STEPPER_DETAILS, true);
+    },
+    closeDropdown() {
+      this.$store.dispatch(MODULE_NAME + '/' + SET_SHOW_MOBILE_STEPPER_DETAILS, false);
+    }
   }
-  
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.scrollable-content {
+.component-container {
   flex: 1;
-  padding: 1.5em 0em;
-  min-height: 2em;
-  height: 65px;
-  min-width: 650px; /* For mobile */
+  padding: 2em 0em;
+  min-height: 65px;
+  /* min-width: 650px; */
 }
 .progress-bar-container {
   background-color: #adb5bd;
-  height: .5rem;
-  border-radius: .25rem;
+  border-radius: 0.25rem;
+  transform: translateY(-2px);
+  height: 0.5rem;
 }
 .progress-bar {
   height: 100%;
-  border-radius: .25rem;
+  border-radius: 0.25rem;
+  background: #036;
 }
 .step-container {
   display: flex;
   justify-content: space-around;
 }
+.step-container a {
+  cursor: pointer;
+}
 .step {
   position: relative;
-  -webkit-transform: translateX(-.5em);
-  transform: translateX(-.5em);
-  margin-top: .25rem;
+  -webkit-transform: translateX(-0.5em);
+  transform: translateX(-0.5em);
+  margin-top: 2px;
 }
 .step:before {
   content: " ";
@@ -114,30 +174,130 @@ export default {
   height: 1em;
   border-radius: 100%;
   background: #fff;
-  border: 3px solid #036;
+  border: 2px solid #606060;
   right: 0;
   left: 0;
   margin: 0 auto;
   bottom: 100%;
+}
+.step-selected:before {
+  border: 2px solid #036;
+}
+.step-passed:before {
+  border: 2px solid #036;
+  background: #036;
 }
 .step-text {
   position: absolute;
   -webkit-transform: translateX(-37%);
   transform: translateX(-37%);
   white-space: nowrap;
-  color: #164d80;
+  color: #494949;
+  font-weight: normal;
 }
-.mobile-step {
+.step-text-selected {
+  font-weight: bold;
+}
+.mobile-progress-bar-container {
+  display: none;
+  font-weight: bold;
+  height: 260px;
+  position: relative;
+  padding-top: 16px;
+}
+.mobile-step-container {
   display: none;
   font-weight: bold;
 }
-@media only screen and (max-width: 575px) {
-  .progress-bar-component {
-    overflow-x: scroll;
+@media only screen and (max-width: 480px) {
+  .component-container {
+    padding: 0;
   }
-  .scrollable-content {
-    padding-left: 15px;
-    padding-right: 15px;
+  .progress-bar-container,
+  .step-container {
+    display: none;
   }
+  .mobile-progress-bar-container,
+  .mobile-step-container {
+    display: block;
+  }
+}
+
+.hide {
+  display: none;
+}
+
+.chevron-container {
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  height: 100%;
+}
+
+.v-step-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  position: absolute;
+  height: 60%;
+}
+.v-step-container a {
+  cursor: pointer;
+}
+.v-step {
+  position: relative;
+  margin-top: 0.25rem;
+}
+.v-step:before {
+  content: " ";
+  position: absolute;
+  width: 1em;
+  height: 1em;
+  border-radius: 100%;
+  background: #fff;
+  border: 2px solid #606060;
+  right: 0;
+  left: 0;
+  margin: 0 auto;
+  bottom: 100%;
+}
+.v-step-selected:before {
+  border: 2px solid #036;
+}
+.v-step-passed:before {
+  border: 2px solid #036;
+  background: #036;
+}
+.v-step-text {
+  position: absolute;
+  -webkit-transform: translateY(-80%);
+  transform: translateY(-80%);
+  white-space: nowrap;
+  color: #494949;
+  left: 30px;
+  font-weight: normal;
+}
+.v-step-text-selected {
+  font-weight: bold;
+}
+.v-progress-bar-container {
+  background-color: #606060;
+  width: 0.25rem;
+  border-radius: 0.25rem;
+  position: absolute;
+  height: 60%;
+  left: 22px;
+}
+.v-progress-bar {
+  width: 100%;
+  border-radius: 0.25rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  color: #fff;
+  text-align: center;
+  white-space: nowrap;
+  background-color: #036;
+  transition: height 0.6s ease;
 }
 </style>
--- oop/src/store/index.js
+++ oop/src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import app from './modules/app';
 import form from './modules/form';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
+    app,
     form
   }
 });
