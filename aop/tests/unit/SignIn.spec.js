80d25f7dc3eeb52173f35a0bcb124660458a13c3
Ian-c-cameron <ian.c.cam@gmail.com>
2021-05-04 13:52:11 -0700

    start creating test: signIn component

---
+++ aop/tests/unit/SignIn.spec.js
@@ -0,0 +1,18 @@
+import { expect } from "chai";
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import VueRouter from 'vue-router';
+import SignIn from "../../src/views/SignIn.vue";
+
+// const localVue = createLocalVue();
+// localVue.use(VueRouter);
+// const router = new VueRouter;
+
+// describe("SignIn.vue", () => {
+//   it("renders", () => {
+//     const wrapper = shallowMount(SignIn, {
+//       localVue,
+//       router
+//     });
+//     expect(wrapper.text()).to.include("BC Services Card");
+//   });
+// });
