cf3eb2c59053d41a4c7a9df4c3e533d882262e41
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2021-05-11 10:17:50 -0700

    add optional sessions

--- bcsc-service/src/routes/api.js
+++ bcsc-service/src/routes/api.js
@@ -11,12 +11,18 @@ module.exports = function (config) {
   });
 
   router.post('/logout', (req, res) => {
-    req.session.destroy();
+    if (req.session)
+      req.session.destroy();
     res.status(204).end();
   });
 
+  // Returns user info using token saved in session, if any
   router.get('/userinfo', (req, res) => {
     // console.log("session = ", req.session);
+    if (!req.session) {
+      return res.json({});
+    }
+
     const token = req.session.token;
     if (!token) {
       return res.json({ error: "auth" });
@@ -41,7 +47,8 @@ module.exports = function (config) {
 
     auth.getToken(config, code)
       .then(token => {
-        req.session.token = token;
+        if (req.session)
+          req.session.token = token;
         return auth.getInfo(config, token);
       })
       .then(info => {
