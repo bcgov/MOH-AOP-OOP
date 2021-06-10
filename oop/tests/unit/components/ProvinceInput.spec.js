ae7cfae758fa93b0db61406cf0b945f8a0eebf30
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-06-10 10:25:40 -0700

    ProvinceInput render, emit tests

---
+++ oop/tests/unit/components/ProvinceInput.spec.js
@@ -0,0 +1,30 @@
+import { mount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import Vue from 'vue';
+import Vuelidate from 'vuelidate';
+import Component from '@/components/ProvinceInput.vue';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+Vue.use(Vuelidate);
+
+describe('ProvinceInput.vue', () => {
+//This is a Shallow Mount as opposed to a regular mount because this test only checks for rendering
+  it('renders', () => {
+    const wrapper = mount(Component, {
+      localVue,
+    });
+    expect(wrapper.element).toBeDefined();
+  });
+  it('emits input correctly through built in method', () => {
+    const wrapper = mount(Component, {
+      localVue,
+    });
+    const fakeEvent="potato";
+    wrapper.vm.inputHandler(fakeEvent);
+    // wrapper.vm.$emit('foo');
+    expect(wrapper.emitted().input).toBeTruthy()
+    expect(wrapper.emitted().input).toEqual([["potato"]]
+    )
+  });
+});
