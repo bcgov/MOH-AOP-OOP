1e1b228a26577d2a2e5ecf2d8f44b0d7f85221d5
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-06-11 13:02:06 -0700

    stateInput render, emit input tests

--- oop/tests/unit/components/CountryInput.spec.js
+++ oop/tests/unit/components/CountryInput.spec.js
@@ -2,27 +2,28 @@ import { mount, createLocalVue } from '@vue/test-utils';
 import Vuex from 'vuex';
 import Vue from 'vue';
 import Vuelidate from 'vuelidate';
-import Component from '@/components/ProvinceInput.vue';
+import Component from '@/components/CountryInput.vue';
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
 Vue.use(Vuelidate);
 
-describe('ProvinceInput.vue', () => {
+describe('CountryInput.vue', () => {
   it('renders', () => {
     const wrapper = mount(Component, {
       localVue,
     });
     expect(wrapper.element).toBeDefined();
   });
-  it('emits input correctly through built in method', () => {
+  it('emits input correctly through built in method', async () => {
     const wrapper = mount(Component, {
       localVue,
     });
-    const fakeEvent="potato";
-    wrapper.vm.inputHandler(fakeEvent);
+    await wrapper.setData({ 
+      country: 'PlaceholderCountry',
+    });
     expect(wrapper.emitted().input).toBeTruthy()
-    expect(wrapper.emitted().input).toEqual([["potato"]]
+    expect(wrapper.emitted().input).toEqual([['PlaceholderCountry']]
     )
   });
 });
--- oop/tests/unit/components/StateInput.spec.js
+++ oop/tests/unit/components/StateInput.spec.js
@@ -2,27 +2,26 @@ import { mount, createLocalVue } from '@vue/test-utils';
 import Vuex from 'vuex';
 import Vue from 'vue';
 import Vuelidate from 'vuelidate';
-import Component from '@/components/ProvinceInput.vue';
+import Component from '@/components/StateInput.vue';
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
 Vue.use(Vuelidate);
 
-describe('ProvinceInput.vue', () => {
+describe('StateInput.vue', () => {
   it('renders', () => {
     const wrapper = mount(Component, {
       localVue,
     });
     expect(wrapper.element).toBeDefined();
   });
-  it('emits input correctly through built in method', () => {
+  it('emits input correctly through built in method', async () => {
     const wrapper = mount(Component, {
       localVue,
     });
-    const fakeEvent="potato";
-    wrapper.vm.inputHandler(fakeEvent);
+    wrapper.vm.inputHandler('PlaceholderRegion');
     expect(wrapper.emitted().input).toBeTruthy()
-    expect(wrapper.emitted().input).toEqual([["potato"]]
+    expect(wrapper.emitted().input).toEqual([['PlaceholderRegion']]
     )
   });
 });
