027d8c183a92ee6ffcc03ffefb391af41d3f36d2
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-07-08 10:00:34 -0700

    YourInfoPage.vue() spec fix formatting

--- oop/tests/unit/views/YourInfoPage.spec.js
+++ oop/tests/unit/views/YourInfoPage.spec.js
@@ -44,162 +44,159 @@ const SET_DEPENDENT_PHNS = "setDependentPhns";
 const SET_SUBMISSION_RESPONSE = "setSubmissionResponse";
 const SET_SUBMISSION_ERROR = "setSubmissionError";
 
-
-  
 const mutations = {
-    setApplicationUuid(state, payload) {
-      state.applicationUuid = payload;
-    },
-    setCaptchaToken(state, payload) {
-      state.captchaToken = payload;
-    },
-    setSubmissionDate(state, payload) {
-      state.submissionDate = payload;
-    },
-    setReferenceNumber(state, payload) {
-      state.referenceNumber = payload;
-    },
-    setLastName(state, payload) {
-      state.lastName = payload;
-    },
-    setPhn(state, payload) {
-      state.phn = payload;
-    },
-    setPhone(state, payload) {
-      state.phone = payload;
-    },
-    setMoveFromBCDate(state, payload) {
-      state.moveFromBCDate = payload;
-    },
-    setArriveDestinationDate(state, payload) {
-      state.arriveDestinationDate = payload;
-    },
-    setIsNewAddressKnown(state, payload) {
-      state.isNewAddressKnown = payload;
-    },
-    setCountry(state, payload) {
-      state.country = payload;
-    },
-    setAddressLines(state, payload) {
-      state.addressLines = payload;
-    },
-    setProvince(state, payload) {
-      state.province = payload;
-    },
-    setCity(state, payload) {
-      state.city = payload;
-    },
-    setPostalCode(state, payload) {
-      state.postalCode = payload;
-    },
-    setAccountType(state, payload) {
-      state.accountType = payload;
-    },
-    setPersonMoving(state, payload) {
-      state.personMoving = payload;
-    },
-    setIsAllDependentsMoving(state, payload) {
-      state.isAllDependentsMoving = payload;
-    },
-    setDependentPhns(state, payload) {
-      state.dependentPhns = payload;
-    },
-    setSubmissionResponse(state, payload) {
-      state.submissionResponse = payload;
-    },
-    setSubmissionError(state, payload) {
-      state.submissionError = payload;
-    },
-  }
+  setApplicationUuid(state, payload) {
+    state.applicationUuid = payload;
+  },
+  setCaptchaToken(state, payload) {
+    state.captchaToken = payload;
+  },
+  setSubmissionDate(state, payload) {
+    state.submissionDate = payload;
+  },
+  setReferenceNumber(state, payload) {
+    state.referenceNumber = payload;
+  },
+  setLastName(state, payload) {
+    state.lastName = payload;
+  },
+  setPhn(state, payload) {
+    state.phn = payload;
+  },
+  setPhone(state, payload) {
+    state.phone = payload;
+  },
+  setMoveFromBCDate(state, payload) {
+    state.moveFromBCDate = payload;
+  },
+  setArriveDestinationDate(state, payload) {
+    state.arriveDestinationDate = payload;
+  },
+  setIsNewAddressKnown(state, payload) {
+    state.isNewAddressKnown = payload;
+  },
+  setCountry(state, payload) {
+    state.country = payload;
+  },
+  setAddressLines(state, payload) {
+    state.addressLines = payload;
+  },
+  setProvince(state, payload) {
+    state.province = payload;
+  },
+  setCity(state, payload) {
+    state.city = payload;
+  },
+  setPostalCode(state, payload) {
+    state.postalCode = payload;
+  },
+  setAccountType(state, payload) {
+    state.accountType = payload;
+  },
+  setPersonMoving(state, payload) {
+    state.personMoving = payload;
+  },
+  setIsAllDependentsMoving(state, payload) {
+    state.isAllDependentsMoving = payload;
+  },
+  setDependentPhns(state, payload) {
+    state.dependentPhns = payload;
+  },
+  setSubmissionResponse(state, payload) {
+    state.submissionResponse = payload;
+  },
+  setSubmissionError(state, payload) {
+    state.submissionError = payload;
+  },
+};
 const actions = {
-    resetForm({ commit }) {
-      commit(SET_APPLICATION_UUID, null);
-      commit(SET_CAPTCHA_TOKEN, null);
-      commit(SET_SUBMISSION_DATE, null);
-      commit(SET_REFERENCE_NUMBER, null);
-      commit(SET_LAST_NAME, null);
-      commit(SET_PHN, null);
-      commit(SET_PHONE, null);
-      commit(SET_MOVE_FROM_BC_DATE, null);
-      commit(SET_ARRIVE_DESTINATION_DATE, null);
-      commit(SET_IS_NEW_ADDRESS_KNOWN, null);
-      commit(SET_COUNTRY, null);
-      commit(SET_ADDRESS_LINES, []);
-      commit(SET_PROVINCE, null);
-      commit(SET_CITY, null);
-      commit(SET_POSTAL_CODE, null);
-      commit(SET_ACCOUNT_TYPE, null);
-      commit(SET_PERSON_MOVING, null);
-      commit(SET_IS_ALL_DEPENDENTS_MOVING, null);
-      commit(SET_DEPENDENT_PHNS, []);
-      commit(SET_SUBMISSION_RESPONSE, null);
-      commit(SET_SUBMISSION_ERROR, null);
-    },
-    setApplicationUuid({ commit }, applicationUuid) {
-      commit(SET_APPLICATION_UUID, applicationUuid);
-    },
-    setCaptchaToken({ commit }, captchaToken) {
-      commit(SET_CAPTCHA_TOKEN, captchaToken);
-    },
-    setSubmissionDate({ commit }, submissionDate) {
-      commit(SET_SUBMISSION_DATE, submissionDate);
-    },
-    setReferenceNumber({ commit }, referenceNumber) {
-      commit(SET_REFERENCE_NUMBER, referenceNumber);
-    },
-    setLastName({ commit }, lastName) {
-      commit(SET_LAST_NAME, lastName);
-    },
-    setPhn({ commit }, phn) {
-      commit(SET_PHN, phn);
-    },
-    setPhone({ commit }, phone) {
-      commit(SET_PHONE, phone);
-    },
-    setMoveFromBCDate({ commit }, moveFromBCDate) {
-      commit(SET_MOVE_FROM_BC_DATE, moveFromBCDate);
-    },
-    setArriveDestinationDate({ commit }, arriveDestinationDate) {
-      commit(SET_ARRIVE_DESTINATION_DATE, arriveDestinationDate);
-    },
-    setIsNewAddressKnown({ commit }, isNewAddressKnown) {
-      commit(SET_IS_NEW_ADDRESS_KNOWN, isNewAddressKnown);
-    },
-    setCountry({ commit }, country) {
-      commit(SET_COUNTRY, country);
-    },
-    setAddressLines({ commit }, addressLines) {
-      commit(SET_ADDRESS_LINES, addressLines);
-    },
-    setProvince({ commit }, province) {
-      commit(SET_PROVINCE, province);
-    },
-    setCity({ commit }, city) {
-      commit(SET_CITY, city);
-    },
-    setPostalCode({ commit }, postalCode) {
-      commit(SET_POSTAL_CODE, postalCode);
-    },
-    setAccountType({ commit }, accountType) {
-      commit(SET_ACCOUNT_TYPE, accountType);
-    },
-    setPersonMoving({ commit }, personMoving) {
-      commit(SET_PERSON_MOVING, personMoving);
-    },
-    setIsAllDependentsMoving({ commit }, isAllDependentsMoving) {
-      commit(SET_IS_ALL_DEPENDENTS_MOVING, isAllDependentsMoving);
-    },
-    setDependentPhns({ commit }, dependentPhns) {
-      commit(SET_DEPENDENT_PHNS, dependentPhns);
-    },
-    setApiResponse({ commit }, response) {
-      commit(SET_SUBMISSION_RESPONSE, response);
-    },
-    setApiError({ commit }, error) {
-      commit(SET_SUBMISSION_ERROR, error);
-    },
-  }
-
+  resetForm({ commit }) {
+    commit(SET_APPLICATION_UUID, null);
+    commit(SET_CAPTCHA_TOKEN, null);
+    commit(SET_SUBMISSION_DATE, null);
+    commit(SET_REFERENCE_NUMBER, null);
+    commit(SET_LAST_NAME, null);
+    commit(SET_PHN, null);
+    commit(SET_PHONE, null);
+    commit(SET_MOVE_FROM_BC_DATE, null);
+    commit(SET_ARRIVE_DESTINATION_DATE, null);
+    commit(SET_IS_NEW_ADDRESS_KNOWN, null);
+    commit(SET_COUNTRY, null);
+    commit(SET_ADDRESS_LINES, []);
+    commit(SET_PROVINCE, null);
+    commit(SET_CITY, null);
+    commit(SET_POSTAL_CODE, null);
+    commit(SET_ACCOUNT_TYPE, null);
+    commit(SET_PERSON_MOVING, null);
+    commit(SET_IS_ALL_DEPENDENTS_MOVING, null);
+    commit(SET_DEPENDENT_PHNS, []);
+    commit(SET_SUBMISSION_RESPONSE, null);
+    commit(SET_SUBMISSION_ERROR, null);
+  },
+  setApplicationUuid({ commit }, applicationUuid) {
+    commit(SET_APPLICATION_UUID, applicationUuid);
+  },
+  setCaptchaToken({ commit }, captchaToken) {
+    commit(SET_CAPTCHA_TOKEN, captchaToken);
+  },
+  setSubmissionDate({ commit }, submissionDate) {
+    commit(SET_SUBMISSION_DATE, submissionDate);
+  },
+  setReferenceNumber({ commit }, referenceNumber) {
+    commit(SET_REFERENCE_NUMBER, referenceNumber);
+  },
+  setLastName({ commit }, lastName) {
+    commit(SET_LAST_NAME, lastName);
+  },
+  setPhn({ commit }, phn) {
+    commit(SET_PHN, phn);
+  },
+  setPhone({ commit }, phone) {
+    commit(SET_PHONE, phone);
+  },
+  setMoveFromBCDate({ commit }, moveFromBCDate) {
+    commit(SET_MOVE_FROM_BC_DATE, moveFromBCDate);
+  },
+  setArriveDestinationDate({ commit }, arriveDestinationDate) {
+    commit(SET_ARRIVE_DESTINATION_DATE, arriveDestinationDate);
+  },
+  setIsNewAddressKnown({ commit }, isNewAddressKnown) {
+    commit(SET_IS_NEW_ADDRESS_KNOWN, isNewAddressKnown);
+  },
+  setCountry({ commit }, country) {
+    commit(SET_COUNTRY, country);
+  },
+  setAddressLines({ commit }, addressLines) {
+    commit(SET_ADDRESS_LINES, addressLines);
+  },
+  setProvince({ commit }, province) {
+    commit(SET_PROVINCE, province);
+  },
+  setCity({ commit }, city) {
+    commit(SET_CITY, city);
+  },
+  setPostalCode({ commit }, postalCode) {
+    commit(SET_POSTAL_CODE, postalCode);
+  },
+  setAccountType({ commit }, accountType) {
+    commit(SET_ACCOUNT_TYPE, accountType);
+  },
+  setPersonMoving({ commit }, personMoving) {
+    commit(SET_PERSON_MOVING, personMoving);
+  },
+  setIsAllDependentsMoving({ commit }, isAllDependentsMoving) {
+    commit(SET_IS_ALL_DEPENDENTS_MOVING, isAllDependentsMoving);
+  },
+  setDependentPhns({ commit }, dependentPhns) {
+    commit(SET_DEPENDENT_PHNS, dependentPhns);
+  },
+  setApiResponse({ commit }, response) {
+    commit(SET_SUBMISSION_RESPONSE, response);
+  },
+  setApiError({ commit }, error) {
+    commit(SET_SUBMISSION_ERROR, error);
+  },
+};
 
 const mockResponse = {
   data: {
@@ -369,9 +366,15 @@ jest.mock("axios", () => ({
   post: jest.fn(),
 }));
 
-jest.spyOn(logService, "logNavigation").mockImplementation(() => Promise.resolve("logged"));
-jest.spyOn(logService, "logError").mockImplementation(() => Promise.resolve("logged"));
-jest.spyOn(logService, "logInfo").mockImplementation(() => Promise.resolve("logged"));
+jest
+  .spyOn(logService, "logNavigation")
+  .mockImplementation(() => Promise.resolve("logged"));
+jest
+  .spyOn(logService, "logError")
+  .mockImplementation(() => Promise.resolve("logged"));
+jest
+  .spyOn(logService, "logInfo")
+  .mockImplementation(() => Promise.resolve("logged"));
 
 describe("YourInfoPage.vue", () => {
   let state;
@@ -578,7 +581,6 @@ describe("YourInfoPage.vue phoneValidator()", () => {
 });
 
 describe("YourInfoPage.vue nextPage()", () => {
-
   it("throws an error, does not call api service when last name is not present", async () => {
     const store = new Vuex.Store({
       modules: {
@@ -652,7 +654,7 @@ describe("YourInfoPage.vue nextPage()", () => {
           },
           namespaced: true,
           mutations,
-          actions
+          actions,
         },
       },
     });
