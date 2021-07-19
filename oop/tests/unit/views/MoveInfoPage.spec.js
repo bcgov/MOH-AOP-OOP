7b8198e0f953654011d010f9a9fede0727f19d50
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-07-19 10:08:58 -0700

    partially working MoveInfoPage.vue addAddressField() tests

--- oop/tests/unit/views/MoveInfoPage.spec.js
+++ oop/tests/unit/views/MoveInfoPage.spec.js
@@ -1,13 +1,67 @@
 import { shallowMount, createLocalVue } from "@vue/test-utils";
 import Vuex from "vuex";
 // import VueRouter from "vue-router";
-import Component from "@/views/HomePage.vue";
+import Component from "@/views/MoveInfoPage.vue";
 // import spaEnvService from "@/services/spa-env-service";
 import pageStateService from "@/services/page-state-service";
 import formTemplate from "@/store/modules/form";
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
+
+const mutations = formTemplate.mutations;
+const actions = formTemplate.actions;
+const state = formTemplate.state;
+const dataTemplate = {
+  moveFromBCDate: null,
+  arriveDestinationDate: null,
+  isNewAddressKnown: null,
+  addressLines: [],
+  country: null,
+  province: null,
+  city: null,
+  postalCode: null,
+  showServerValidationError: false,
+  isPageLoaded: false,
+  isLoading: false,
+  currNumOfAddressLines: null,
+  isNewAddressKnownRadioItems: [
+    {
+      id: "is-new-address-known-y",
+      label: "Yes",
+      value: "Y",
+    },
+    {
+      id: "is-new-address-known-n",
+      label: "No",
+      value: "N",
+    },
+  ],
+};
+
+// const stateTemplate  = {
+//   applicationUuid: null,
+//   captchaToken: null,
+//   lastName: null,
+//   phn: null,
+//   phone: null,
+//   moveFromBCDate: null,
+//   arriveDestinationDate: null,
+//   isNewAddressKnown: null,
+//   country: null,
+//   addressLines: [],
+//   province: null,
+//   city: null,
+//   postalCode: null,
+//   accountType: null,
+//   personMoving: null,
+//   isAllDependentsMoving: null,
+//   dependentPhns: [],
+//   submissionDate: null,
+//   referenceNumber: null,
+//   submissionResponse: null,
+//   submissionError: null,
+// }
 // Vue.use(Vuelidate);
 
 // const scrollHelper = require("@/helpers/scroll");
@@ -26,18 +80,84 @@ jest.mock("@/helpers/scroll", () => ({
 // const spyOnScrollTo = jest.spyOn(scrollHelper, "scrollTo");
 
 describe("MoveInfoPage.vue", () => {
-  const mutations = formTemplate.mutations;
-  const actions = formTemplate.actions;
-  let state;
   let store;
 
   beforeEach(() => {
-    state = {
-      lastName: null,
-      phn: null,
-      phone: null,
-    };
+    store = new Vuex.Store({
+      modules: {
+        form: {
+          mutations,
+          actions,
+          state: {
+            applicationUuid: null,
+            captchaToken: null,
+            lastName: null,
+            phn: null,
+            phone: null,
+            moveFromBCDate: null,
+            arriveDestinationDate: null,
+            isNewAddressKnown: null,
+            country: null,
+            addressLines: [],
+            province: null,
+            city: null,
+            postalCode: null,
+            accountType: null,
+            personMoving: null,
+            isAllDependentsMoving: null,
+            dependentPhns: [],
+            submissionDate: null,
+            referenceNumber: null,
+            submissionResponse: null,
+            submissionError: null,
+          },
+          namespaced: true,
+        },
+      },
+    });
+  });
 
+  it("renders", () => {
+    const wrapper = shallowMount(Component, {
+      localVue,
+      store,
+      data: () => {
+        return {
+          moveFromBCDate: null,
+          arriveDestinationDate: null,
+          isNewAddressKnown: null,
+          addressLines: [],
+          country: null,
+          province: null,
+          city: null,
+          postalCode: null,
+          showServerValidationError: false,
+          isPageLoaded: false,
+          isLoading: false,
+          currNumOfAddressLines: null,
+          isNewAddressKnownRadioItems: [
+            {
+              id: "is-new-address-known-y",
+              label: "Yes",
+              value: "Y",
+            },
+            {
+              id: "is-new-address-known-n",
+              label: "No",
+              value: "N",
+            },
+          ],
+        }
+      },
+    });
+    expect(wrapper.element).toBeDefined();
+  });
+});
+
+describe.skip("MoveInfoPage.vue addAddressField", () => {
+  let store;
+
+  beforeEach(() => {
     store = new Vuex.Store({
       modules: {
         form: {
@@ -50,11 +170,18 @@ describe("MoveInfoPage.vue", () => {
     });
   });
 
-  it("renderss", () => {
+  it.skip("adds a line to the addressLines array in data", async () => {
     const wrapper = shallowMount(Component, {
       localVue,
-      store
+      store,
+      data: () => dataTemplate,
     });
-    expect(wrapper.element).toBeDefined();
+
+    expect(wrapper.vm.addressLines).toEqual([]);
+
+    wrapper.vm.addAddressField();
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.vm.addressLines).toHaveLength(1);
   });
 });
