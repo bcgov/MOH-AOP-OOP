bdacef46e449fb6a747bce357941feb806d8773c
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-07-27 14:49:21 -0700

    Helpers address.js tests with address data

--- oop/tests/unit/helpers/address.spec.js
+++ oop/tests/unit/helpers/address.spec.js
@@ -38,23 +38,60 @@ describe("Helper address.js", () => {
   });
 
   it.skip("does not turn trailing spaces into individual elements", () => {
-    const result = Function(
-      ["AAAAA ", "BBBBB ", "CCCCC "],
-      2
-    );
+    const result = Function(["AAAAA ", "BBBBB ", "CCCCC "], 2);
 
     expect(result).toEqual(["AAAAA", "BBBBB", "CCCCC"]);
     expect(result).not.toEqual(["AAAAA", "", "BBBBB", "", "CCCCC", ""]);
   });
 
-  it("returns an array with elements split up by spaces when they're longer than the parameter passed along", () => {
+  it("returns an array with elements split up by spaces when the array elements longer than the parameter passed along", () => {
     const result = Function(
       ["AAAAA BBBBB CCCCC", "DDDDD EEEEE", "FFFFF GGGGG"],
       2
     );
 
-    expect(result).toEqual(["AAAAA", "BBBBB", "CCCCC", "DDDDD", "EEEEE", "FFFFF", "GGGGG"]);
+    expect(result).toEqual([
+      "AAAAA",
+      "BBBBB",
+      "CCCCC",
+      "DDDDD",
+      "EEEEE",
+      "FFFFF",
+      "GGGGG",
+    ]);
   });
 
+  it("does not split up array elements that are shorter than the parameter passed along", () => {
+    const result = Function(
+      ["AAAAA BBBBB CCCCC", "DDDDD EEEEE", "FFFFF GGGGG"],
+      12
+    );
 
+    expect(result).toEqual([
+      "AAAAA BBBBB",
+      "CCCCC",
+      "DDDDD EEEEE",
+      "FFFFF GGGGG",
+    ]);
+  });
+
+  it("works on data formatted like addresses", () => {
+    const result = Function(
+      ["123 Pekwachnamaykoskwaskwaypinwanik Lake", "Unit 123"],
+      12
+    );
+
+    expect(result).toEqual([
+      "123",
+      "Pekwachnamaykoskwaskwaypinwanik",
+      "Lake",
+      "Unit 123",
+    ]);
+  });
+
+  it("works on data formatted like addresses (case 2)", () => {
+    const result = Function(["716 Yates Street Victoria, BC V8W 1K4"], 12);
+
+    expect(result).toEqual(["716 Yates", "Street", "Victoria, BC", "V8W 1K4"]);
+  });
 });
