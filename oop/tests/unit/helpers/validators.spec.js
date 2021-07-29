259826ce9c38af2ce73275c69615df2c76243003
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-07-29 15:27:26 -0700

    Helpers validators.js the rest of the tests

--- oop/tests/unit/helpers/validators.spec.js
+++ oop/tests/unit/helpers/validators.spec.js
@@ -73,3 +73,75 @@ describe("Helper validators.js nonBCValidator()", () => {
     expect(result).toEqual(true);
   });
 });
+
+describe("Helper validators.js hasEmptyPostalCode()", () => {
+  it("returns a boolean", () => {
+    const result = Helper.hasEmptyPostalCode();
+    expect(result).toBeDefined();
+    expect(typeof result).toEqual("boolean");
+  });
+
+  it("returns true if passed a null value", () => {
+    const result = Helper.hasEmptyPostalCode(null);
+    expect(result).toEqual(true);
+  });
+
+  it("returns true if passed an empty string", () => {
+    const result = Helper.hasEmptyPostalCode("");
+    expect(result).toEqual(true);
+  });
+
+  it("returns false if passed a postal code", () => {
+    const result = Helper.hasEmptyPostalCode("H0H 0H0");
+    expect(result).toEqual(false);
+  });
+
+  it("returns false if passed an undefined value", () => {
+    const result = Helper.hasEmptyPostalCode(undefined);
+    expect(result).toEqual(false);
+  });
+});
+
+describe("Helper validators.js invalidCharValidator()", () => {
+  it("returns true if passed null value", () => {
+    const result = Helper.invalidCharValidator(null);
+    expect(result).toEqual(true);
+  });
+
+  it("returns true if passed empty string", () => {
+    const result = Helper.invalidCharValidator("");
+    expect(result).toEqual(true);
+  });
+
+  //function is not currently in use, so further tests were not written
+});
+
+describe("Helper validators.js nonBCPostalCodeValidator()", () => {
+  it("returns a boolean", () => {
+    const result = Helper.nonBCPostalCodeValidator();
+    expect(result).toBeDefined();
+    expect(typeof result).toEqual("boolean");
+  });
+
+  it("returns false if given a BC postal code", () => {
+    const result = Helper.nonBCPostalCodeValidator("V1V 1V1");
+    expect(result).toEqual(false);
+  });
+
+  it("returns true if given a non-BC postal code", () => {
+    const result = Helper.nonBCPostalCodeValidator("H0H 0H0");
+    expect(result).toEqual(true);
+  });
+});
+
+describe("Helper validators.js canadaPostalCodeLengthValidator()", () => {
+  it("returns true if given a string seven characters long", () => {
+    const result = Helper.canadaPostalCodeLengthValidator("V1V 1V1");
+    expect(result).toEqual(true);
+  });
+
+  it("returns false if given a string not seven characters long", () => {
+    const result = Helper.canadaPostalCodeLengthValidator("H0H0H0");
+    expect(result).toEqual(false);
+  });
+});
