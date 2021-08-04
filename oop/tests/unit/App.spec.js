7790b5a90473728807c668fd0052a67653dae6e9
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-08-04 16:08:33 -0700

    App.vue render test

--- oop/tests/unit/App.spec.js
+++ oop/tests/unit/App.spec.js
@@ -1,17 +1,21 @@
 import { mount, createLocalVue } from "@vue/test-utils";
 import Vuex from "vuex";
 import Vue from "vue";
+import VueRouter from "vue-router";
 import Vuelidate from "vuelidate";
 import Component from "@/App.vue";
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
+localVue.use(VueRouter);
 Vue.use(Vuelidate);
+const router = new VueRouter();
 
 describe("App.vue", () => {
   it("renders", () => {
     const wrapper = mount(Component, {
       localVue,
+      router
     });
     expect(wrapper.element).toBeDefined();
   });
