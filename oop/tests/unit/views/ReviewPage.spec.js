8412bc1a11b811cffd5c517d0dd650b2a56cc680
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-08-05 14:49:11 -0700

    ReviewPage.vue submitForm() dispatch test

--- oop/tests/unit/views/ReviewPage.spec.js
+++ oop/tests/unit/views/ReviewPage.spec.js
@@ -3,22 +3,23 @@ import Vuex from "vuex";
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Vuelidate from "vuelidate";
-// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-// import pageStateService from "@/services/page-state-service";
 import { cloneDeep } from "lodash";
-// import * as stepRoutes from "@/router/step-routes";
-// import { routeStepOrder } from "@/router/routes";
 import * as formTemplate from "@/store/modules/form";
-import * as appTemplate from "@/store/modules/app";
 import Component from "@/views/ReviewPage.vue";
+import axios from "axios";
+
+jest.mock("axios", () => ({
+  get: jest.fn(),
+  post: jest.fn(() => {
+    return Promise.resolve();
+  }),
+}));
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
 localVue.use(VueRouter);
 Vue.use(Vuelidate);
-// Vue.component("font-awesome-icon", FontAwesomeIcon);
 const router = new VueRouter();
-
 
 describe("ReviewPage.vue", () => {
   let wrapper;
@@ -28,7 +29,6 @@ describe("ReviewPage.vue", () => {
     store = new Vuex.Store({
       modules: {
         form: cloneDeep(formTemplate),
-        app: cloneDeep(appTemplate),
       },
     });
     wrapper = shallowMount(Component, {
@@ -45,5 +45,61 @@ describe("ReviewPage.vue", () => {
 
   it("renders", () => {
     expect(wrapper.element).toBeDefined();
+  });
+});
+
+describe("ReviewPage.vue submitForm()", () => {
+  let wrapper;
+  let store;
+
+  const dummyFormData = {
+    applicationUuid: "defaultUuid",
+    captchaToken: "defaultCaptcha",
+    lastName: "defaultLastname",
+    phn: "defaultPhn",
+    phone: "defaultPhone",
+    moveFromBCDate: new Date(2021, 11, 17),
+    arriveDestinationDate: new Date(2021, 11, 18),
+    isNewAddressKnown: false,
+    country: "fake-istan",
+    addressLines: ["123 fake street", "unit 111"],
+    province: "fakeprovince",
+    city: "fakesville",
+    postalCode: "H0H0H0",
+    accountType: "AH",
+    personMoving: null,
+    isAllDependentsMoving: null,
+    dependentPhns: [],
+    submissionDate: null,
+    referenceNumber: null,
+    submissionResponse: null,
+    submissionError: null,
+  };
+
+  let tempForm = cloneDeep(formTemplate.default);
+  tempForm.state = dummyFormData;
+
+  beforeEach(() => {
+    store = new Vuex.Store({
+      modules: {
+        form: tempForm,
+      },
+    });
+    wrapper = shallowMount(Component, {
+      localVue,
+      store,
+      router,
+    });
+  });
+
+  afterEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+  });
+
+  it("dispatches", () => {
+    const spyOnDispatch = jest.spyOn(wrapper.vm.$store, "dispatch");
+    wrapper.vm.submitForm();
+    expect(spyOnDispatch).toHaveBeenCalled();
   });
 });
