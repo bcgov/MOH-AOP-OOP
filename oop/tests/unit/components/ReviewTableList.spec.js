09d874d57732f91acec6109afc1f7c961b0292a6
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-08-10 14:20:07 -0700

    ReviewTableList.spec.js refactor to eliminate duplicate lines of code

--- oop/tests/unit/components/ReviewTableList.spec.js
+++ oop/tests/unit/components/ReviewTableList.spec.js
@@ -4,6 +4,7 @@ import Vue from "vue";
 import Vuelidate from "vuelidate";
 import Component from "@/components/ReviewTableList.vue";
 import { formatDate } from "@/helpers/date";
+import * as formTemplate from "@/store/modules/form";
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
@@ -40,39 +41,6 @@ const dummyDataNull = {
   city: null,
   postalCode: null,
 };
-
-// const MODULE_NAME = 'form';
-// const RESET_FORM = 'resetForm';
-
-const SET_APPLICATION_UUID = "setApplicationUuid";
-const SET_CAPTCHA_TOKEN = "setCaptchaToken";
-const SET_SUBMISSION_DATE = "setSubmissionDate";
-const SET_REFERENCE_NUMBER = "setReferenceNumber";
-
-// Your info page:
-const SET_LAST_NAME = "setLastName";
-const SET_PHN = "setPhn";
-const SET_PHONE = "setPhone";
-
-// Move Info page
-const SET_MOVE_FROM_BC_DATE = "setMoveFromBCDate";
-const SET_ARRIVE_DESTINATION_DATE = "setArriveDestinationDate";
-const SET_IS_NEW_ADDRESS_KNOWN = "setIsNewAddressKnown";
-const SET_COUNTRY = "setCountry";
-const SET_ADDRESS_LINES = "setAddressLines";
-const SET_PROVINCE = "setProvince";
-const SET_CITY = "setCity";
-const SET_POSTAL_CODE = "setPostalCode";
-
-// Account type page:
-const SET_ACCOUNT_TYPE = "setAccountType";
-const SET_PERSON_MOVING = "setPersonMoving";
-const SET_IS_ALL_DEPENDENTS_MOVING = "setIsAllDependentsMoving";
-const SET_DEPENDENT_PHNS = "setDependentPhns";
-
-// Sending page:
-const SET_SUBMISSION_RESPONSE = "setSubmissionResponse";
-const SET_SUBMISSION_ERROR = "setSubmissionError";
 
 const storeTemplateNull = {
   state: () => {
@@ -135,159 +103,9 @@ const storeTemplate = {
 
     return state;
   },
-  mutations: {
-    setApplicationUuid(state, payload) {
-      state.applicationUuid = payload;
-    },
-    setCaptchaToken(state, payload) {
-      state.captchaToken = payload;
-    },
-    setSubmissionDate(state, payload) {
-      state.submissionDate = payload;
-    },
-    setReferenceNumber(state, payload) {
-      state.referenceNumber = payload;
-    },
-    setLastName(state, payload) {
-      state.lastName = payload;
-    },
-    setPhn(state, payload) {
-      state.phn = payload;
-    },
-    setPhone(state, payload) {
-      state.phone = payload;
-    },
-    setMoveFromBCDate(state, payload) {
-      state.moveFromBCDate = payload;
-    },
-    setArriveDestinationDate(state, payload) {
-      state.arriveDestinationDate = payload;
-    },
-    setIsNewAddressKnown(state, payload) {
-      state.isNewAddressKnown = payload;
-    },
-    setCountry(state, payload) {
-      state.country = payload;
-    },
-    setAddressLines(state, payload) {
-      state.addressLines = payload;
-    },
-    setProvince(state, payload) {
-      state.province = payload;
-    },
-    setCity(state, payload) {
-      state.city = payload;
-    },
-    setPostalCode(state, payload) {
-      state.postalCode = payload;
-    },
-    setAccountType(state, payload) {
-      state.accountType = payload;
-    },
-    setPersonMoving(state, payload) {
-      state.personMoving = payload;
-    },
-    setIsAllDependentsMoving(state, payload) {
-      state.isAllDependentsMoving = payload;
-    },
-    setDependentPhns(state, payload) {
-      state.dependentPhns = payload;
-    },
-    setSubmissionResponse(state, payload) {
-      state.submissionResponse = payload;
-    },
-    setSubmissionError(state, payload) {
-      state.submissionError = payload;
-    },
-  },
-  actions: {
-    resetForm({ commit }) {
-      commit(SET_APPLICATION_UUID, null);
-      commit(SET_CAPTCHA_TOKEN, null);
-      commit(SET_SUBMISSION_DATE, null);
-      commit(SET_REFERENCE_NUMBER, null);
-      commit(SET_LAST_NAME, null);
-      commit(SET_PHN, null);
-      commit(SET_PHONE, null);
-      commit(SET_MOVE_FROM_BC_DATE, null);
-      commit(SET_ARRIVE_DESTINATION_DATE, null);
-      commit(SET_IS_NEW_ADDRESS_KNOWN, null);
-      commit(SET_COUNTRY, null);
-      commit(SET_ADDRESS_LINES, []);
-      commit(SET_PROVINCE, null);
-      commit(SET_CITY, null);
-      commit(SET_POSTAL_CODE, null);
-      commit(SET_ACCOUNT_TYPE, null);
-      commit(SET_PERSON_MOVING, null);
-      commit(SET_IS_ALL_DEPENDENTS_MOVING, null);
-      commit(SET_DEPENDENT_PHNS, []);
-      commit(SET_SUBMISSION_RESPONSE, null);
-      commit(SET_SUBMISSION_ERROR, null);
-    },
-    setApplicationUuid({ commit }, applicationUuid) {
-      commit(SET_APPLICATION_UUID, applicationUuid);
-    },
-    setCaptchaToken({ commit }, captchaToken) {
-      commit(SET_CAPTCHA_TOKEN, captchaToken);
-    },
-    setSubmissionDate({ commit }, submissionDate) {
-      commit(SET_SUBMISSION_DATE, submissionDate);
-    },
-    setReferenceNumber({ commit }, referenceNumber) {
-      commit(SET_REFERENCE_NUMBER, referenceNumber);
-    },
-    setLastName({ commit }, lastName) {
-      commit(SET_LAST_NAME, lastName);
-    },
-    setPhn({ commit }, phn) {
-      commit(SET_PHN, phn);
-    },
-    setPhone({ commit }, phone) {
-      commit(SET_PHONE, phone);
-    },
-    setMoveFromBCDate({ commit }, moveFromBCDate) {
-      commit(SET_MOVE_FROM_BC_DATE, moveFromBCDate);
-    },
-    setArriveDestinationDate({ commit }, arriveDestinationDate) {
-      commit(SET_ARRIVE_DESTINATION_DATE, arriveDestinationDate);
-    },
-    setIsNewAddressKnown({ commit }, isNewAddressKnown) {
-      commit(SET_IS_NEW_ADDRESS_KNOWN, isNewAddressKnown);
-    },
-    setCountry({ commit }, country) {
-      commit(SET_COUNTRY, country);
-    },
-    setAddressLines({ commit }, addressLines) {
-      commit(SET_ADDRESS_LINES, addressLines);
-    },
-    setProvince({ commit }, province) {
-      commit(SET_PROVINCE, province);
-    },
-    setCity({ commit }, city) {
-      commit(SET_CITY, city);
-    },
-    setPostalCode({ commit }, postalCode) {
-      commit(SET_POSTAL_CODE, postalCode);
-    },
-    setAccountType({ commit }, accountType) {
-      commit(SET_ACCOUNT_TYPE, accountType);
-    },
-    setPersonMoving({ commit }, personMoving) {
-      commit(SET_PERSON_MOVING, personMoving);
-    },
-    setIsAllDependentsMoving({ commit }, isAllDependentsMoving) {
-      commit(SET_IS_ALL_DEPENDENTS_MOVING, isAllDependentsMoving);
-    },
-    setDependentPhns({ commit }, dependentPhns) {
-      commit(SET_DEPENDENT_PHNS, dependentPhns);
-    },
-    setApiResponse({ commit }, response) {
-      commit(SET_SUBMISSION_RESPONSE, response);
-    },
-    setApiError({ commit }, error) {
-      commit(SET_SUBMISSION_ERROR, error);
-    },
-  },
+  mutations: formTemplate.mutations
+  ,
+  actions: formTemplate.actions,
   getters: {},
 };
 
