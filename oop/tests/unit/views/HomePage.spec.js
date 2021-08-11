f0d378f78e0dde627d5d835d0e2af8f85cd26ab8
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-08-11 15:56:12 -0700

    preparing HomePage.spec.js for created() tests

--- oop/tests/unit/views/HomePage.spec.js
+++ oop/tests/unit/views/HomePage.spec.js
@@ -61,6 +61,61 @@ const spaEnvServiceResponse = {
   request: {},
 };
 
+const spaEnvServiceMaintenance = {
+  data: {
+    SPA_ENV_OOP_MAINTENANCE_FLAG: "true",
+    SPA_ENV_OOP_MAINTENANCE_START: "2021-05-20 11:00:00 AM",
+    SPA_ENV_OOP_MAINTENANCE_END: "2021-05-20 11:00:00 PM",
+    SPA_ENV_OOP_MAINTENANCE_MESSAGE:
+      "This application is temporarily unavailable. We apologize for any inconvenience. Please try again later.",
+    SPA_ENV_OOP_ENABLE_ADDRESS_VALIDATOR: "true",
+  },
+  status: 200,
+  statusText: "OK",
+  headers: {
+    "access-control-allow-credentials": "true",
+    "access-control-allow-headers":
+      "Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Requested-With",
+    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "access-control-allow-origin": "https://my.gov.bc.ca",
+    "access-control-expose-headers": "Authorization",
+    "cache-control": "no-store",
+    connection: "close",
+    "content-length": "339",
+    "content-security-policy":
+      "default-src * data: blob: filesystem: 'unsafe-inline' 'unsafe-eval'",
+    "content-type": "application/json; charset=utf-8",
+    date: "Fri, 16 Jul 2021 17:13:20 GMT",
+    etag: 'W/"153-GDCv0dFpnRzks5Q4aJXEeiTyqxw"',
+    pragma: "no-cache",
+    server: "nginx",
+    "strict-transport-security": "max-age=86400; includeSubDomains",
+    "x-content-type-options": "nosniff",
+    "x-frame-options": "DENY",
+    "x-powered-by": "Express",
+    "x-xss-protection": "1",
+  },
+  config: {
+    url: "/oop/api/env",
+    method: "post",
+    data: null,
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/x-www-form-urlencoded",
+      SPA_ENV_NAME:
+        '{"SPA_ENV_OOP_MAINTENANCE_FLAG":"","SPA_ENV_OOP_MAINTENANCE_START":"","SPA_ENV_OOP_MAINTENANCE_END":"","SPA_ENV_OOP_MAINTENANCE_MESSAGE":"","SPA_ENV_OOP_ENABLE_ADDRESS_VALIDATOR":""}',
+    },
+    transformRequest: [null],
+    transformResponse: [null],
+    timeout: 0,
+    xsrfCookieName: "XSRF-TOKEN",
+    xsrfHeaderName: "X-XSRF-TOKEN",
+    maxContentLength: -1,
+    maxBodyLength: -1,
+  },
+  request: {},
+};
+
 const localVue = createLocalVue();
 localVue.use(Vuex);
 
@@ -254,5 +309,43 @@ describe("HomePage.vue nextPage()", () => {
 
     expect(pageStateService.setPageComplete).toHaveBeenCalled();
     expect(pageStateService.visitPage).toHaveBeenCalled();
+  });
+});
+
+describe("HomePage.vue created()", () => {
+  const mutations = formTemplate.mutations;
+  const actions = formTemplate.actions;
+  let state;
+  let store;
+
+  beforeEach(() => {
+    state = {
+      lastName: null,
+      phn: null,
+      phone: null,
+    };
+
+    store = new Vuex.Store({
+      modules: {
+        form: {
+          mutations,
+          actions,
+          state,
+          namespaced: true,
+        },
+      },
+    });
+
+    jest
+      .spyOn(spaEnvService, "loadEnvs")
+      .mockImplementationOnce(() => Promise.resolve(spaEnvServiceMaintenance));
+  });
+
+  it("renders", () => {
+    const wrapper = shallowMount(Component, {
+      store,
+      localVue,
+    });
+    expect(wrapper.element).toBeDefined();
   });
 });
