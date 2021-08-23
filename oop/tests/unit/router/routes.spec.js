04456a96fc65ac38d41e817a284992d1bd7e9c22
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-08-23 10:36:02 -0700

    fix alias in routes.spec.js

--- oop/tests/unit/router/routes.spec.js
+++ oop/tests/unit/router/routes.spec.js
@@ -1,35 +1,35 @@
-import { isPastPath as testFunction } from "@/router/routes.js";
+import { isPastPath } from "@/router/routes.js";
 import { routeStepOrder } from "@/router/routes.js";
 
 const stepOrderLength = routeStepOrder.length;
 
 describe("Helper routes.js isPastPath", () => {
   it("returns false on invalid data", () => {
-    const result = testFunction("default1", "default2");
+    const result = isPastPath("default1", "default2");
 
     expect(result).toEqual(false);
   });
 
   it("returns false if argument 1 doesn't exist in the path order but argument 2 does", () => {
-    const result = testFunction("default1", routeStepOrder[0].path);
+    const result = isPastPath("default1", routeStepOrder[0].path);
 
     expect(result).toEqual(false);
   });
 
   it("returns true if argument 2 doesn't exist in the path order but argument 1 does", () => {
-    const result = testFunction(routeStepOrder[0].path, "default2");
+    const result = isPastPath(routeStepOrder[0].path, "default2");
 
     expect(result).toEqual(true);
   });
 
   it("returns true if first argument is before the second argument in step order", () => {
-    const result = testFunction(routeStepOrder[0].path, routeStepOrder[1].path);
+    const result = isPastPath(routeStepOrder[0].path, routeStepOrder[1].path);
 
     expect(result).toEqual(true);
   });
 
   it("returns true if first argument is before the second argument in step order (case 2)", () => {
-    const result = testFunction(
+    const result = isPastPath(
       routeStepOrder[stepOrderLength - 2].path,
       routeStepOrder[stepOrderLength - 1].path
     );
@@ -38,13 +38,13 @@ describe("Helper routes.js isPastPath", () => {
   });
 
   it("returns false if second argument is before the first argument in step order", () => {
-    const result = testFunction(routeStepOrder[1].path, routeStepOrder[0].path);
+    const result = isPastPath(routeStepOrder[1].path, routeStepOrder[0].path);
 
     expect(result).toEqual(false);
   });
 
   it("returns false if second argument is before the first argument in step order (case 2)", () => {
-    const result = testFunction(
+    const result = isPastPath(
       routeStepOrder[stepOrderLength - 1].path,
       routeStepOrder[stepOrderLength - 2].path
     );
