a710508842b5396e2c1a96c93452e926463ee270
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-08-23 14:46:32 -0700

    first somewhat functioning end to end test

--- oop/tests/e2e/specs/test.spec.js
+++ oop/tests/e2e/specs/test.spec.js
@@ -1,20 +1,10 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-// describe('My First Test', () => {
-//   it('Does not do much!', () => {
-//     expect(true).to.equal(true)
-//   })
-
-//   it('Visits the app root url', () => {
-//     cy.visit('/oop')
-
-//     cy.contains('b', 'I have read and understand this information')
-//     expect(true).toEqual(true)
-//   })
-// })
-
 describe('My First Test', () => {
-  it('Does not do much!', () => {
+  it('Visits the app root url', () => {
+    cy.visit('http://localhost:8080/oop')
+
+    cy.contains('b', 'I have read and understand this information')
     expect(true).to.equal(true)
   })
 })
