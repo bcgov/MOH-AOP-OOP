da6428c41d0bf8387b54346f91d2ea73438a1dce
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-08-23 09:48:55 -0700

    change alias for test functions

--- oop/tests/unit/helpers/string.spec.js
+++ oop/tests/unit/helpers/string.spec.js
@@ -1,34 +1,34 @@
-import { replaceSpecialCharacters as testFunction } from "@/helpers/string.js";
+import { replaceSpecialCharacters } from "@/helpers/string.js";
 
 describe("Helper string.js", () => {
   it("returns a string when passed a string", () => {
-    const result = testFunction("string");
+    const result = replaceSpecialCharacters("string");
     expect(result).toBeDefined();
     expect(typeof result).toEqual("string");
   });
 
   it("returns null when called without an argument", () => {
-    const result = testFunction();
+    const result = replaceSpecialCharacters();
     expect(result).toBeNull();
   });
 
   it("replaces special characters with similar equivalents", () => {
-    const result = testFunction("Á");
+    const result = replaceSpecialCharacters("Á");
     expect(result).toEqual("A");
   });
 
   it("replaces special characters with similar equivalents (case 2)", () => {
-    const result = testFunction(" Ę ");
+    const result = replaceSpecialCharacters(" Ę ");
     expect(result).toEqual(" E ");
   });
 
   it("replaces special characters with similar equivalents (case 3)", () => {
-    const result = testFunction("aꝋa");
+    const result = replaceSpecialCharacters("aꝋa");
     expect(result).toEqual("aoa");
   });
 
   it("leaves numerals passed through in a string alone", () => {
-    const result = testFunction("2");
+    const result = replaceSpecialCharacters("2");
     expect(result).toEqual("2");
   });
 });
--- oop/tests/unit/helpers/us-states.spec.js
+++ oop/tests/unit/helpers/us-states.spec.js
@@ -1,36 +1,36 @@
-import { getStateNameFromCode as testFunction } from "@/helpers/us-states.js";
+import { getStateNameFromCode } from "@/helpers/us-states.js";
 
 describe("Helper us-states.js", () => {
   it("returns a string when passed a string", () => {
-    const result = testFunction("LA");
+    const result = getStateNameFromCode("LA");
     expect(result).toBeDefined();
     expect(typeof result).toEqual("string");
   });
 
   it("returns undefined when passed null", () => {
-    const result = testFunction();
+    const result = getStateNameFromCode();
     expect(result).toBeUndefined();
   });
 
   it("returns non-string arguments passed into it", () => {
-    const result = testFunction(["default"]);
+    const result = getStateNameFromCode(["default"]);
     expect(result).toEqual(["default"]);
   });
 
   it("returns a full state name when passed a state code", () => {
-    const result = testFunction("LA");
+    const result = getStateNameFromCode("LA");
     expect(result).toEqual("Louisiana");
   });
 
   it("returns a full state name when passed a state code (case 2)", () => {
-    const result = testFunction("AE");
+    const result = getStateNameFromCode("AE");
     expect(result).toEqual(
       "Armed Forces Europe, Canada, Africa and Middle East"
     );
   });
 
   it("returns a full state name when passed a state code (case 3)", () => {
-    const result = testFunction("PR");
+    const result = getStateNameFromCode("PR");
     expect(result).toEqual("Puerto Rico");
   });
 });
