95ca325ee6373e3967ec01c33cd7370c84dbf8ed
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-08-25 13:59:05 -0700

    add data-cy selectors for month, day, and year to happy path test. also add code that programmatically determines year

--- oop/tests/e2e/specs/test.spec.js
+++ oop/tests/e2e/specs/test.spec.js
@@ -4,6 +4,14 @@ Cypress.on("uncaught:exception", (err, runnable) => {
   return false;
 });
 
+//At last check, there is code in the program that checks for dates in the distant future/past
+//To prevent code written in 2021 from failing in 2025 because the date is too far back,
+//this code pull the current year and adds one.
+//This is to hopefully increase the stability of the test
+
+const testYear = new Date().getFullYear() + 1
+// const testYear = thisYear + 1;
+
 describe("Happy path", () => {
   it("completes the app lifecycle without errors", () => {
     //Home page
@@ -49,6 +57,14 @@ describe("Happy path", () => {
       expect(loc.href).to.eq("http://localhost:8080/oop/move-info");
       expect(loc.pathname).to.eq("/oop/move-info");
     });
+    //Dates
+    cy.get("[data-cy=moveFromBCDateMonth]").select("January");
+    cy.get("[data-cy=moveFromBCDateDay]").type("11");
+    cy.get("[data-cy=moveFromBCDateYear]").type(testYear);
+    cy.get("[data-cy=arriveDestinationDateMonth]").select("January");
+    cy.get("[data-cy=arriveDestinationDateDay]").type("12");
+    cy.get("[data-cy=arriveDestinationDateYear]").type(testYear);
+    //Address
     /* ***********************please fix this to be a data-cy link instead ****************** */
     // cy.get('[dataCy=accountTypeAH]').eq(0).click()
     cy.get("[id=is-new-address-known-n]").click({ force: true });
