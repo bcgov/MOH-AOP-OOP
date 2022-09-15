7eaf56ff788c85ad5c009f70c7f892c6487634aa
Stephanie Harrison <stephanie.harrison@maximuscanada.ca>
2022-09-15 11:31:37 -0700

    fix console warnings

--- oop/tests/unit/App.spec.js
+++ oop/tests/unit/App.spec.js
@@ -1,18 +1,38 @@
-import { mount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 import { createRouter, createWebHistory } from "vue-router";
 import { routeCollection } from "@/router/index";
 import Component from "@/App.vue";
+import { createStore } from "vuex";
+import * as formTemplate from "@/store/modules/form";
+import { cloneDeep } from "lodash";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: routeCollection,
 });
 
+let tempForm = cloneDeep(formTemplate.default);
+let store = createStore({
+  modules: {
+    form: tempForm,
+  },
+});
+
 describe("App.vue", () => {
   it("renders", () => {
-    const wrapper = mount(Component, {
+    const wrapper = shallowMount(Component, {
       global: {
-        plugins: [router],
+        plugins: [store, router],
+      },
+      data: () => {
+        return {
+          focusableEls: [],
+          focusedEl: null,
+          captchaAPIBasePath: "/oop/api/captcha",
+          applicationUuid: "111",
+          isCaptchaValid: false,
+          isTermsAccepted: false,
+        };
       },
     });
     expect(wrapper.element).toBeDefined();
