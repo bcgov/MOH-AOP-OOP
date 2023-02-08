83816e91bfb9e524c311d4365234f5ca8fb5ad80
Stephanie Harrison <stephanie.harrison@maximuscanada.ca>
2023-02-08 14:05:27 -0800

    fix formatting in Maintenance.spec.js for consistency

--- aop/tests/unit/Maintenance.spec.js
+++ aop/tests/unit/Maintenance.spec.js
@@ -1,20 +1,21 @@
-import { mount } from '@vue/test-utils';
-import Maintenance from '../../src/views/Maintenance.vue';
-import { createStore } from 'vuex';
-import router from '../../src/router/index';
-import store from '../../src/store/index';
+import { mount } from "@vue/test-utils";
+import Maintenance from "../../src/views/Maintenance.vue";
+import { createStore } from "vuex";
+import router from "../../src/router/index";
+import store from "../../src/store/index";
 
 const testStore = createStore({
-  ...store
+  ...store,
 });
 
-describe('Maintenance.vue', () => {
+describe("Maintenance.vue", () => {
   // This is a Shallow Mount as opposed to a regular mount because this test only checks for rendering
-  it('renders', () => {
+  it("renders", () => {
     const wrapper = mount(Maintenance, {
-    global: {
-      plugins: [router, testStore]
-    }});
+      global: {
+        plugins: [router, testStore],
+      },
+    });
     expect(wrapper.element).toBeDefined();
   });
 });
