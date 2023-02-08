c2cc4dc63c8be31e99dd44b0058b17ce6306308b
Stephanie Harrison <stephanie.harrison@maximuscanada.ca>
2023-02-08 14:20:59 -0800

    fix formatting, add mocks to Review.spec.js

--- aop/tests/unit/Review.spec.js
+++ aop/tests/unit/Review.spec.js
@@ -1,20 +1,26 @@
-import { mount } from '@vue/test-utils';
-import Review from '../../src/views/Review.vue';
-import { createStore } from 'vuex';
-import router from '../../src/router/index';
-import store from '../../src/store/index';
+import { shallowMount } from "@vue/test-utils";
+import Review from "../../src/views/Review.vue";
+import { createStore } from "vuex";
+import router from "../../src/router/index";
+import store from "../../src/store/index";
 
 const testStore = createStore({
-  ...store
+  ...store,
 });
 
-describe('Review.vue', () => {
+jest.mock("@/helpers/scroll", () => ({
+  scrollTo: jest.fn(),
+  scrollToError: jest.fn(),
+}));
+
+describe("Review.vue", () => {
   // This is a Shallow Mount as opposed to a regular mount because this test only checks for rendering
-  it('renders', () => {
-    const wrapper = mount(Review, {
-    global: {
-      plugins: [router, testStore]
-    }});
+  it("renders", () => {
+    const wrapper = shallowMount(Review, {
+      global: {
+        plugins: [router, testStore],
+      },
+    });
     expect(wrapper.element).toBeDefined();
   });
 });
