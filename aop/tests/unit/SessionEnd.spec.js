3009dfeaa286cc9284e8f23c0040db32438b37d6
Stephanie Harrison <stephanie.harrison@maximuscanada.ca>
2023-02-09 10:03:46 -0800

    fix formatting in SessionEnd.spec.js

--- aop/tests/unit/SessionEnd.spec.js
+++ aop/tests/unit/SessionEnd.spec.js
@@ -1,20 +1,21 @@
-import { mount } from '@vue/test-utils';
-import SessionEnd from '../../src/views/SessionEnd.vue';
-import { createStore } from 'vuex';
-import router from '../../src/router/index';
-import store from '../../src/store/index';
+import { mount } from "@vue/test-utils";
+import SessionEnd from "../../src/views/SessionEnd.vue";
+import { createStore } from "vuex";
+import router from "../../src/router/index";
+import store from "../../src/store/index";
 
 const testStore = createStore({
-  ...store
+  ...store,
 });
 
-describe('SessionEnd.vue', () => {
+describe("SessionEnd.vue", () => {
   // This is a Shallow Mount as opposed to a regular mount because this test only checks for rendering
-  it('renders', () => {
+  it("renders", () => {
     const wrapper = mount(SessionEnd, {
-    global: {
-      plugins: [router, testStore]
-    }});
+      global: {
+        plugins: [router, testStore],
+      },
+    });
     expect(wrapper.element).toBeDefined();
   });
 });
