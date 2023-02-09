ee8e2d3ad23ea6b9eadcd5ad588a3bb64f2f0b6e
Stephanie Harrison <stephanie.harrison@maximuscanada.ca>
2023-02-09 09:54:28 -0800

    fix formatting, add mocks to Sending.spec.js

--- aop/tests/unit/Sending.spec.js
+++ aop/tests/unit/Sending.spec.js
@@ -1,20 +1,36 @@
-import { mount } from '@vue/test-utils';
-import Sending from '../../src/views/Sending.vue';
-import { createStore } from 'vuex';
-import router from '../../src/router/index';
-import store from '../../src/store/index';
+import { mount } from "@vue/test-utils";
+import Sending from "../../src/views/Sending.vue";
+import { createStore } from "vuex";
+import router from "../../src/router/index";
+import store from "../../src/store/index";
 
 const testStore = createStore({
-  ...store
+  ...store,
+  mutations: {
+    setUUID() {
+      jest.fn()
+    },
+  }
 });
 
-describe('Sending.vue', () => {
+jest.mock("@/helpers/scroll", () => ({
+  scrollTo: jest.fn(),
+  scrollToError: jest.fn(),
+}));
+
+jest.mock("@/helpers/scroll", () => ({
+  scrollTo: jest.fn(),
+  scrollToError: jest.fn(),
+}));
+
+describe("Sending.vue", () => {
   // This is a Shallow Mount as opposed to a regular mount because this test only checks for rendering
-  it('renders', () => {
+  it("renders", () => {
     const wrapper = mount(Sending, {
-    global: {
-      plugins: [router, testStore]
-    }});
+      global: {
+        plugins: [router, testStore],
+      },
+    });
     expect(wrapper.element).toBeDefined();
   });
 });
