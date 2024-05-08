03e68b6b4ef9d4f5f2110f8efdf0a6acac378fa8
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2024-05-08 08:17:03 -0700

    formatting fixes in e2e test

--- oop/tests/e2e/specs/happypath2.spec.js
+++ oop/tests/e2e/specs/happypath2.spec.js
@@ -301,8 +301,8 @@ describe("Happy path", () => {
     cy.get("[data-cy=zipCode]").clear();
     cy.get("[data-cy=zipCode]")
       .type("1234567") // 7 characters, invalid
-      .should('have.value', 123456) // 6 characters, valid (max length mask worked correctly)
-      .should('not.have.value', 1234567) // 7 characters, invalid
+      .should("have.value", 123456) // 6 characters, valid (max length mask worked correctly)
+      .should("not.have.value", 1234567); // 7 characters, invalid
 
     //USA ready to continue
     cy.get("[data-cy=usaOtherStreetAddress]").clear();
@@ -352,8 +352,8 @@ describe("Happy path", () => {
     cy.get("[data-cy=zipCode]").clear();
     cy.get("[data-cy=zipCode]")
       .type("abcdefghijklmnopqrstuvw") // 23 characters, invalid
-      .should('have.value', 'abcdefghijklmnopqrstuv') // 22 characters, valid (max length mask worked correctly)
-      .should('not.have.value', 'abcdefghijklmnopqrstuvw') // 23 characters, invalid
+      .should("have.value", "abcdefghijklmnopqrstuv") // 22 characters, valid (max length mask worked correctly)
+      .should("not.have.value", "abcdefghijklmnopqrstuvw"); // 23 characters, invalid
 
     //Other ready to continue
     cy.get("[data-cy=usaOtherStreetAddress]").clear();
