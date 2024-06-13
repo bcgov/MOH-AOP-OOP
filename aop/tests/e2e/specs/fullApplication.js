0a6b55e9a54d7bd25d98248421addda3c192f97c
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2024-06-13 08:37:12 -0700

    add location names to e2e test to prevent test stalling out

--- aop/tests/e2e/specs/fullApplication.js
+++ aop/tests/e2e/specs/fullApplication.js
@@ -5,6 +5,9 @@ describe('Full AOP application flow', () => {
   it('Clicks through log in page', () => {
     cy.visit('/');
     cy.get('.bcgov-button').click();
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq("/aop/submission-info");
+    });
     cy.get('h1').contains('Select a form')
   });
 
@@ -15,12 +18,20 @@ describe('Full AOP application flow', () => {
 
   it('Continues to the review page', () => {
     cy.get('.bcgov-button').contains('Continue').click();
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq("/aop/review");
+    });
     cy.get('h1').contains('Review your submission');
   })
 
   it('Successfully submits', () => {
     cy.get('.bcgov-button').contains('Submit').click();
-    cy.wait(1000)
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq("/aop/sending");
+    });
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq("/aop/confirmation");
+    });
     cy.get('h1').contains('Confirmation message');
   })
 });
