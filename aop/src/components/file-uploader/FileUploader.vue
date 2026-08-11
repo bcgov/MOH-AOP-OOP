6753a46fcf474277d2482c1a892e1bd1f531e6b8
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2026-08-11 14:32:55 -0700

    refactor comments in fileuploader

--- aop/src/components/file-uploader/FileUploader.vue
+++ aop/src/components/file-uploader/FileUploader.vue
@@ -33,11 +33,9 @@
 
 <script>
 import Button from '../Button.vue';
-// import * as PDFJS from 'pdfjs-dist/legacy/build/pdf';
-// import pdfJsWorker from 'pdfjs-dist/legacy/build/pdf.worker.entry';
-import * as PDFJS from "pdfjs-dist";
+import * as PDFJS from 'pdfjs-dist/legacy/build/pdf.mjs';
 //the legacy worker build includes a polyfill for Promise.withResolvers
-// import pdfjsWorker from "pdfjs-dist/legacy/build/pdf.worker?url";
+import pdfjsWorker from "pdfjs-dist/build/pdf.worker.mjs?url";
 import sha1 from 'sha1';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -48,9 +46,7 @@ import 'mdn-polyfills/MouseEvent';
 import 'mdn-polyfills/HTMLCanvasElement.prototype.toBlob';
 import '../../polyfills/DOMMatrix';
 
-// PDFJS.workerSrc = pdfjsWorker;
-// PDFJS.disableWorker = true;
-// PDFJS.disableStream = true;
+pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
 const MIN_IMAGE_SIZE_BYTES = 20000;
 const MAX_IMAGE_SIZE_BYTES = 1048576;
