417846fca87405aee10f8934a67194fcc2f77856
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2026-08-11 14:33:29 -0700

    vitest migration

--- aop/jest.setup.js
+++
@@ -1,6 +0,0 @@
-import { config } from "@vue/test-utils";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-config.global.components = {
-  "font-awesome-icon": FontAwesomeIcon,
-};
