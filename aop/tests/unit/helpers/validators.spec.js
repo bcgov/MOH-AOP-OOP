e805b734aa0bf438fe75b2bf17a0d9e31e90ce4a
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2026-09-08 16:07:07 -0700

    add unit tests for isValidEmail

--- aop/tests/unit/helpers/validators.spec.js
+++ aop/tests/unit/helpers/validators.spec.js
@@ -1,4 +1,4 @@
-import { isValidPhone } from "@/helpers/validators.js";
+import { isValidPhone, isValidEmail } from "@/helpers/validators.js";
 
 describe("isValidPhone", () => {
   it("returns false when called with no arguments", () => {
@@ -35,6 +35,57 @@ describe("isValidPhone", () => {
   });
   it("returns true when called with valid phone number (with a +1 at the beginning)", () => {
     const response = isValidPhone("+12501234567");
+    expect(response).toEqual(true);
+  });
+});
+
+describe("isValidEmail", () => {
+  it("returns false when called with no arguments", () => {
+    const response = isValidEmail();
+    expect(response).toEqual(false);
+  });
+  it("returns false when called with an incorrectly formatted email address", () => {
+    const response = isValidEmail("foobar");
+    expect(response).toEqual(false);
+  });
+  it("returns false when called with an incorrectly formatted email address (with commas)", () => {
+    const response = isValidEmail("foobar#foobar,com");
+    expect(response).toEqual(false);
+  });
+  it("returns false when called with an incorrectly formatted email address (extra period in domain)", () => {
+    const response = isValidEmail("foobar@.foober.com");
+    expect(response).toEqual(false);
+  });
+  it("returns false when called with an incorrectly formatted email address (extra period in address)", () => {
+    const response = isValidEmail("foobar..foobar@foobar.com");
+    expect(response).toEqual(false);
+  });
+  it("returns false when called with an incorrectly formatted email address (leading period)", () => {
+    const response = isValidEmail(".foobar@foobar.com");
+    expect(response).toEqual(false);
+  });
+  it("returns false when called with an incorrectly formatted email address (trailing hyphen)", () => {
+    const response = isValidEmail("foobar@foobar-.com");
+    expect(response).toEqual(false);
+  });
+  it("returns false when called with an incorrectly formatted email address (with an @ symbol)", () => {
+    const response = isValidEmail("foobar@foobar");
+    expect(response).toEqual(false);
+  });
+  it("returns true when called with a validly formatted email address", () => {
+    const response = isValidEmail("foobar@foobar.com");
+    expect(response).toEqual(true);
+  });
+  it("returns true when called with a validly formatted email address (with a period in it)", () => {
+    const response = isValidEmail("foobar.foobar@foobar.com");
+    expect(response).toEqual(true);
+  });
+  it("returns true when called with a validly formatted email address (with a plus in it)", () => {
+    const response = isValidEmail("foobar+foobar@foobar.com");
+    expect(response).toEqual(true);
+  });
+  it("returns true when called with a validly formatted email address (with a dash in it)", () => {
+    const response = isValidEmail("foobar-foobar@foobar.com");
     expect(response).toEqual(true);
   });
 });
